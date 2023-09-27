a30f0b9b62b82b6e665e1768e37fc37a4095b144
HTrieuhehe <97211030+HTrieuhehe@users.noreply.github.com>
2023-09-27 22:41:03 +0700

    Add: Send Email Booking

--- SupFAmof.Serviec/DTO/Request/MailSettings.cs
+++ SupFAmof.Serviec/DTO/Request/MailSettings.cs
@@ -34,6 +34,36 @@ namespace SupFAmof.Service.DTO.Request
 
         public int Code { get; set; }
 
-        public string? ContestTestField { get; set; }
+        public MailBookingRequest? MailBookingRequest { get; set; }
+    }
+
+    public class MailBookingRequest
+    {
+        [Required]
+        public string? RegistrationCode { get; set; }
+
+        [Required]
+        public string? PostName { get; set; }
+
+        [Required]
+        public string? DateFrom { get; set; }
+
+        public string? DateTo { get; set; }
+
+        [Required]
+        public string? PositionName { get; set; }
+
+        [Required]
+        public string? TimeFrom { get; set; }
+
+        public string? TimeTo { get; set; }
+
+        [Required]
+        public string? SchoolName { get; set; }
+
+        [Required]
+        public string? Location { get; set; }
+
+        public string? Note { get; set; }
     }
 }

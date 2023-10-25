2bed3952a054e4f0648cd0e37e2f6cb9d5f295a2
HTrieuhehe <97211030+HTrieuhehe@users.noreply.github.com>
2023-10-25 17:04:36 +0700

    Fix: hot fix

---
+++ SupFAmof.Serviec/DTO/Response/Admission/AdmissionFinancialReportResponse.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SupFAmof.Service.DTO.Response.Admission
+{
+    public class CollabInfoReportResponse
+    {
+        public string? Name { get; set; }
+        public string? IdStudent { get; set; }
+        public string? IdentityNumber { get; set; }
+        public string? TaxNumber { get; set; }
+
+        // Final Amaount sum by all salary in Working Content
+        public string? FinalAmount { get; set; }
+    
+        public ICollection<WorkingContentResponse>? WorkingContents { get; set; }
+    }
+
+    public class WorkingContentResponse
+    {
+        public string? PostCategoryDescription { get; set; }
+        public string? DateFrom { get; set; }
+
+        // salary sum by all positions are registed in one post
+        public string? Salary { get; set; }
+
+        //total amount must pay for each Post
+        public string? TotalAmount { get; set; }
+    }
+}
---
+++ SupFAmof.Serviec/Service/FinancialReportService.cs
@@ -0,0 +1,62 @@
+﻿using AutoMapper;
+using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
+using SupFAmof.Data.Entity;
+using SupFAmof.Data.UnitOfWork;
+using SupFAmof.Service.DTO.Response;
+using SupFAmof.Service.DTO.Response.Admission;
+using SupFAmof.Service.Service.ServiceInterface;
+using SupFAmof.Service.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SupFAmof.Service.Service
+{
+    public class FinancialReportService : IFinancialReportService
+    {
+        private readonly IMapper _mapper;
+        private readonly IUnitOfWork _unitOfWork;
+
+        public FinancialReportService(IMapper mapper, IUnitOfWork unitOfWork)
+        {
+            _mapper = mapper;
+            _unitOfWork = unitOfWork;
+        }
+
+        public async Task<BaseResponseViewModel<CollabInfoReportResponse>> GetAdmissionFinancialReport(int accountId)
+        {
+            try
+            {
+                var account = _unitOfWork.Repository<Account>()
+                                         .GetAll()
+                                         .Include(a => a.AccountInformation)
+                                         .Include(b => b.PostAttendees)
+                                            .ThenInclude(c => c.Position)
+                                         .Include(d => d.PostAttendees)
+                                            .ThenInclude(e => e.Post)
+                                            .ThenInclude(f => f.PostCategory)
+                                        .Where(x => x.Posts.Any(x => x.CreateAt >= Ultils.GetCurrentDatetime().AddMonths(-1) && x.AccountId == accountId));
+
+
+                return new BaseResponseViewModel<CollabInfoReportResponse>()
+                {
+                    Status = new StatusViewModel
+                    {
+                        Message = "Success",
+                        ErrorCode = 0,
+                        Success = true,
+                    },
+                    Data = null
+                    //Data = _mapper.Map<CollabInfoReportResponse>(null)
+                };
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+    }
+}

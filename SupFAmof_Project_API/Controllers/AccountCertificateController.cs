e071bec7a0d8a87df3ea6fdf53d58e3cfea6c83a
HTrieuhehe <97211030+HTrieuhehe@users.noreply.github.com>
2023-12-02 15:29:54 +0700

    Get all certificate for collab

--- SupFAmof_Project_API/Controllers/AccountCertificateController.cs
+++ SupFAmof_Project_API/Controllers/AccountCertificateController.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Mvc;
 using SupFAmof.Service.DTO.Request;
 using SupFAmof.Service.DTO.Response;
+using SupFAmof.Service.DTO.Response.Admission;
 using SupFAmof.Service.Exceptions;
 using SupFAmof.Service.Service;
 using SupFAmof.Service.Service.ServiceInterface;
@@ -14,10 +15,12 @@ namespace SupFAmof.API.Controllers
     public class AccountCertificateController : ControllerBase
     {
         private readonly IAccountCertificateService _accountCertificateService;
+        private readonly ITrainingCertificateService _certificateService;
 
-        public AccountCertificateController(IAccountCertificateService accountCertificateService)
+        public AccountCertificateController(IAccountCertificateService accountCertificateService, ITrainingCertificateService certificateService)
         {
             _accountCertificateService = accountCertificateService;
+            _certificateService = certificateService;
         }
 
         /// <summary>
@@ -38,6 +41,35 @@ namespace SupFAmof.API.Controllers
                     return Unauthorized();
                 }
                 return await _accountCertificateService.GetAccountCertificateByAccountId(account.Id, filter, paging);
+            }
+            catch (ErrorResponse ex)
+            {
+                if (ex.Error.StatusCode == 404)
+                {
+                    return NotFound(ex.Error);
+                }
+                return BadRequest(ex.Error);
+            }
+        }
+
+        /// <summary>
+        /// Get Account Certificate by Token 
+        /// </summary>
+        /// <returns></returns>
+        /// 
+        [HttpGet("getAllCertificateFromAdmission")]
+        public async Task<ActionResult<BaseResponsePagingViewModel<TrainingCertificateResponse>>> GetCertificates
+        ([FromQuery] TrainingCertificateResponse filter, [FromQuery] PagingRequest paging)
+        {
+            try
+            {
+                var accessToken = Request.Headers["Authorization"].ToString().Replace("Bearer ", "");
+                var account = FireBaseService.GetUserIdFromHeaderToken(accessToken);
+                if (account.Id == (int)SystemAuthorize.NotAuthorize || account.RoleId != (int)SystemRoleEnum.Collaborator)
+                {
+                    return Unauthorized();
+                }
+                return await _certificateService.GetTrainingCertificates(filter, paging);
             }
             catch (ErrorResponse ex)
             {

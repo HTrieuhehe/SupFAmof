62e64fd6a3eb08fb4e1b40271e0d4961565ed20c
HTrieuhehe <97211030+HTrieuhehe@users.noreply.github.com>
2023-12-11 12:41:22 +0700

    hot fix

--- SupFAmof.Serviec/Service/ContractService.cs
+++ SupFAmof.Serviec/Service/ContractService.cs
@@ -20,7 +20,6 @@ using SupFAmof.Service.Service.ServiceInterface;
 using SupFAmof.Service.Utilities;
 using System;
 using System.Collections.Generic;
-using System.Diagnostics.Contracts;
 using System.Linq;
 using System.Net;
 using System.Net.NetworkInformation;

4816f5a0c5ee433acb5a77052378372ee3b85d3f
HTrieuhehe <97211030+HTrieuhehe@users.noreply.github.com>
2023-12-20 10:40:28 +0700

    hot fix

--- SupFAmof.Serviec/Service/PostService.cs
+++ SupFAmof.Serviec/Service/PostService.cs
@@ -501,7 +501,8 @@ namespace SupFAmof.Service.Service
         {
             try
             {
-                int? totalCount = 0;
+                int totalCount = 0;
+                int totalPositionCount = 0;
                 int? totalAmountPosition = 0;
 
                 var post = _unitOfWork.Repository<Post>().GetAll()
@@ -513,41 +514,87 @@ namespace SupFAmof.Service.Service
 
                 var postResponses = post.Item2.ToList();
 
+                //foreach (var item in postResponses)
+                //{
+                //    // lấy tất cả các position Id của bài post hiện tại
+                //    var postPositionIds = item.PostPositions.Select(p => p.Id).ToList();
+
+                //    // tìm post Registration có position Id trung với các bài post
+                //    var postRegistrations = _unitOfWork.Repository<PostRegistration>()
+                //                                        .GetAll()
+                //                                        .Where(reg => postPositionIds.Contains(reg.PositionId) && reg.Status == (int)PostRegistrationStatusEnum.Confirm)
+                //                                        .ToList();
+
+                //    // Tính toán các trường cần thiết
+                //    item.RegisterAmount = postRegistrations.Count;
+
+                //    foreach (var itemDetail in item.PostPositions)
+                //    {
+                //        //count register amount in post attendee based on position
+                //        totalCount += CountRegisterAmount(itemDetail.Id, postRegistrations);
+
+                //        //transafer data to field in post position
+                //        itemDetail.PositionRegisterAmount = totalCount;
+
+                //        //add number of amount required to total amount of a specific post
+                //        totalAmountPosition += itemDetail.Amount;
+
+                //        // Reset temp variable
+                //        totalCount = 0;
+
+                //        //tìm post update history có status pending
+                //        var updateRegistration = _unitOfWork.Repository<PostRgupdateHistory>()
+                //                .GetAll().Where(x => x.PositionId == itemDetail.Id && x.Status == (int)PostRGUpdateHistoryEnum.Pending);
+
+                //        item.TotalUpdateRegisterAmount += updateRegistration.Count();
+                //    }
+
+                //    //transfer data from position after add to field in post
+                //    item.TotalAmountPosition = totalAmountPosition;
+
+                //    // Reset temp variable
+                //    totalAmountPosition = 0;
+                //}
+
                 foreach (var item in postResponses)
                 {
                     // lấy tất cả các position Id của bài post hiện tại
                     var postPositionIds = item.PostPositions.Select(p => p.Id).ToList();
 
                     // tìm post Registration có position Id trung với các bài post
-                    var postRegistrations = _unitOfWork.Repository<PostRegistration>()
-                                                        .GetAll()
-                                                        .Where(reg => postPositionIds.Contains(reg.PositionId) && reg.Status == (int)PostRegistrationStatusEnum.Confirm)
-                                                        .ToList();
+                    var postRegistrations = await _unitOfWork.Repository<PostRegistration>()
+                            .GetAll()
+                            .Where(reg => postPositionIds.Contains(reg.PositionId))
+                            .ToListAsync();
 
-                    // Tính toán các trường cần thiết
-                    item.RegisterAmount = postRegistrations.Count;
+                    var postRegistrationsTotal = postRegistrations.Where(reg => reg.Status != (int)PostRegistrationStatusEnum.Cancel && reg.Status != (int)PostRegistrationStatusEnum.Quit && reg.Status != (int)PostRegistrationStatusEnum.Reject);
+
+                    item.TotalRegisterAmount = postRegistrationsTotal.Count();
+
+                    var postRegistrationsFiltering = postRegistrations.Where(reg => reg.Status == (int)PostRegistrationStatusEnum.Confirm
+                                                                                         && reg.Status == (int)PostRegistrationStatusEnum.CheckIn
+                                                                                         && reg.Status == (int)PostRegistrationStatusEnum.CheckOut);
+
+                    // tính tổng các registration đã được confirm
+                    item.RegisterAmount = postRegistrationsFiltering.Count();
 
                     foreach (var itemDetail in item.PostPositions)
                     {
                         //count register amount in post attendee based on position
-                        totalCount += CountRegisterAmount(itemDetail.Id, postRegistrations);
+                        totalCount += CountRegisterAmount(itemDetail.Id, postRegistrationsFiltering);
+                        totalPositionCount += CountRegisterAmount(itemDetail.Id, postRegistrations);
 
                         //transafer data to field in post position
                         itemDetail.PositionRegisterAmount = totalCount;
+                        itemDetail.TotalPositionRegisterAmount = totalPositionCount;
 
                         //add number of amount required to total amount of a specific post
                         totalAmountPosition += itemDetail.Amount;
 
-                        // Reset temp variable
+                        //reset temp count
                         totalCount = 0;
-
-                        //tìm post update history có status pending
-                        var updateRegistration = _unitOfWork.Repository<PostRgupdateHistory>()
-                                .GetAll().Where(x => x.PositionId == itemDetail.Id && x.Status == (int)PostRGUpdateHistoryEnum.Pending);
-
-                        item.TotalUpdateRegisterAmount += updateRegistration.Count();
+                        totalPositionCount = 0;
                     }
-
                     //transfer data from position after add to field in post
                     item.TotalAmountPosition = totalAmountPosition;
 
